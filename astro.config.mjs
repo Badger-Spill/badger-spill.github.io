b1ab463d55e88a7b194b0db42c8078194648f651
jamesjulich <51384945+jamesjulich@users.noreply.github.com>
2024-01-10 21:27:11 -0600

    Update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "badger-spill.github.io",
+  site: "https://badger-spill.github.io",
 });
