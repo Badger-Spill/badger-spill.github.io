55edc753b9e8c60abcc9252420e29f0e6fb8ef2a
CassieYu229 <136616041+CassieYu229@users.noreply.github.com>
2026-08-31 04:45:47 -0500

    Repush of new design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://badger-spill.github.io",
 });
